159cc76df21ce07620a15f0326b915953d04ee6d
Jelle Visser <jellevisser1994@hotmail.com>
2018-11-30 16:48:15 +0100

    Fixed label initialization

--- first_part.py
+++ first_part.py
@@ -6,23 +6,24 @@ class Perceptron:
     
     def __init__(self, n):
         self.n = n
-        self.init_weights(n)
-        
-    def init_weights(self, n):
         self.weights = [0] * n
-
+        
+    def train(self, x, y, n_epochs=100):
+        for epoch in range(n_epochs):
+            for pattern in x:
+                
 # generating dataset
-def gen_dataset(p=5,n=20):
+def gen_dataset(p=5, n=20):
     x = np.random.normal(0, 1, p*n)
-    x.shape=(p,n)
-    y=np.random.binomial(1,0.5,p)
-    
+    x.shape = (p,n)
+    y = np.random.choice([-1,1], p, True)
+
     return x,y
 
 if __name__ =="__main__":
-        p = 15 
-        n = 20
-        x, y = gen_dataset(p, n)
-        perceptron = Perceptron(n)
-        
-        print(perceptron.weights)
+    p = 15 
+    n = 20
+    x, y = gen_dataset(p, n)
+    perceptron = Perceptron(n)
+    
+    print(y)
