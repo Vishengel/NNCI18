a04bdb1633d98a59f8586beec8104f479e8131d7
stepazar <stepazar@gmail.com>
2018-12-04 10:44:14 +0100

    Part d and simple chart

--- rosenblatt_perceptron.py
+++ rosenblatt_perceptron.py
@@ -1,6 +1,7 @@
 import numpy as np
-random_number = 2018
-#np.random.seed(random_number)
+import plotly as py
+import plotly.graph_objs as go
+
 
 class Perceptron:
     
@@ -25,7 +26,7 @@ class Perceptron:
                 error = np.dot(self.weights, x[idx]) * y[idx]
 
                 # debug statements
-                #print("Weights: ", self.weights, " - x: ", x[idx], " - y: ", y[idx])
+                # print("Weights: ", self.weights, " - x: ", x[idx], " - y: ", y[idx])
                 #print("Error: ", error)
 
                 # if error is lower than or equal to 0, update weights
@@ -43,6 +44,7 @@ class Perceptron:
             print("Max epochs reached")
         elif success:
             print("Success in ", epoch, " epochs!")
+        return success
 
     # function that returns the output activation given an input pattern
     def present_input(self, input):
@@ -53,7 +55,6 @@ def gen_dataset(p=5, n=20):
     x = np.random.normal(0, 1, p*n)
     x.shape = (p,n)
     y = np.random.choice([-1,1], p, True)
-
     return x,y
 
 # test perceptron by training it on a logical and
@@ -81,37 +82,40 @@ def gen_and_gate():
     print(perceptron.present_input([1, 0]))
     print(perceptron.present_input([1, 1]))
 
-# test perceptron by training it on a logical or
-# training does not work correctly at the moment
-def gen_or_gate():
-    x = [[0, 0],
-         [0, 1],
-         [1, 0],
-         [1, 1]
-         ]
-
-    y = [-1, 1, 1, 1]
-
-    x = np.array(x)
-    y = np.array(y)
-
-    perceptron = Perceptron(2)
-    perceptron.train(x, y, 100)
-
-    print(perceptron.weights)
-
-    print(perceptron.present_input([0, 0]))
-    print(perceptron.present_input([0, 1]))
-    print(perceptron.present_input([1, 0]))
-    print(perceptron.present_input([1, 1]))
 
 if __name__ =="__main__":
     p = 15
     n = 20
-    x, y = gen_dataset(p, n)
-    perceptron = Perceptron(n)
-    perceptron.train(x, y)
+    alpha=np.linspace(0.75,3,3)
+    p=alpha*n
+    nd = 2 # number of datasets
+    success_ratios = np.zeros(len(p))
+    for j in range(0,len(p)): # first loop over values of p
+        for i in range(0,nd): # second loop over different datasets
+            x, y = gen_dataset(int(p[j]), n)
+            perceptron = Perceptron(n)
+            success=perceptron.train(x, y)
+            print("Success:",success)
+            if success == True:
+                success_ratios[j]+=1
+        print("Value of p:",p[j],
+              "\nFraction of successful runs:",success_ratios[j]/nd)
 
     #gen_and_gate()
     #gen_or_gate()
 
+    # Plotting chart
+    trace = go.Scatter(x=alpha, y=success_ratios)
+    layout = go.Layout(
+        title='<b>{}</b>'.format('Dependence of successful runs ratio on alpha'),
+        titlefont=dict(family='Open Sans', size=20),
+        font=dict(family='Open Sans'),
+        xaxis=dict(title='<i>{}</i>'.format('alpha'), titlefont=dict(size=16), tickfont=dict(size=12)),
+        yaxis=dict(title='<i>{}</i>'.format('Success ratio'), titlefont=dict(size=16), tickfont=dict(size=12)),
+        legend=dict(font=dict(size=16), orientation='v'),
+    )
+
+    fig = go.Figure(data=[trace], layout=layout)
+    py.offline.plot(fig,
+                    filename='rosenblatt_perceptron_test.html',
+                    auto_open=False)
